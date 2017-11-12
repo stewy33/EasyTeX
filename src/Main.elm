d699c2e639c07f8c24139afb0f5a9707d50f5cc1
spp2010 <20sri17@gmail.com>
2017-11-12 13:58:53 -0500

    latest

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,14 @@ port module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Renderer exposing (..)
-
+import Html exposing (Html, Attribute, div, fieldset, input, label, text)
+import Html.Attributes exposing (name, style, type_)
+import Html.Events exposing (onClick)
+import Markdown
 
 type alias Model =
-    { body : String
+    { fontSize : String
+    , body : String
     }
 
 
@@ -14,6 +18,8 @@ init : ( Model, Cmd Msg )
 init =
     ( Model "", Cmd.none )
 
+type Msg
+  = FieldValue String | FontSize String
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -29,6 +35,8 @@ update msg model =
         case msg of
             FieldValue str ->
                 ( { body = str }, Cmd.none )
+            FontSize newFontSize ->
+                { model | fontSize = newFontSize }
 
 
 subscriptions : Model -> Sub Msg
@@ -38,7 +46,38 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "page" ] [ div [ class "container" ] <| render model.body ]
+    div [ class "page" ] [ div [ class "container", style [("font-size", model.fontSize)] ] <| render model.body ]
+    div [class "changefont"]
+    [ fontSelect ]
+
+fontSelect : Html msg
+fontSelect =
+  let onChange f = on "change" <| Json.Decode.map f <| Json.Decode.at ["target", "value"] Json.Decode.String
+  select
+    [ style [("padding", "20px")]
+    ]
+    [onChange FontSize]
+    [ viewOption "0.8em",
+      viewOption "1em" ,
+      viewOption "1.2em" ,
+    ]
+
+
+sizeToStyle : FontSize -> Attribute msg
+sizeToStyle fontSize =
+  let
+    size =
+      case fontSize of
+        Small ->
+          "0.8em"
+
+        Medium ->
+          "1em"
+
+        Large ->
+          "1.2em"
+  in
+    style [("font-size", size)]
 
 
 main : Program Never Model Msg
